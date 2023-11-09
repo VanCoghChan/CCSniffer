e65047f17f102c8121b65470cd8adfd4d92857ed
VanCoghChan <chencan232@mails.ucas.ac.cn>
2023-11-09 09:54:41 +0800

    提交view模块

---
+++ views/InterfaceSelectDialogView.py
@@ -0,0 +1,113 @@
+import os
+import sys
+
+import psutil
+import scapy.interfaces
+from PyQt6.QtCharts import QChart, QSplineSeries, QValueAxis, QChartView
+from PyQt6.QtCore import QTimer, Qt, QThread, pyqtSlot, QSize
+from scapy.all import *
+from PyQt6.QtGui import QIcon, QMouseEvent, QPen, QFont, QColor, QPainter, QPixmap
+from PyQt6.QtWidgets import QApplication, QHBoxLayout, QLabel, QListWidgetItem, QWidget, QRadioButton
+from qt_material import apply_stylesheet
+
+from custom.style import min_style, close_style
+from models.GetInterfaceModel import GetInterfaceServer
+from views.primitive_views.InterfaceSelectDialogView_pre import *
+
+
+class InterfaceSelectDialog(Ui_InterfaceSelectDialog, QtWidgets.QDialog):
+    def __init__(self):
+        super(InterfaceSelectDialog, self).__init__()
+        self._interfaces = scapy.interfaces.get_working_ifaces()
+        # 初始化UI
+        self.setupUi(self)
+        # 设置鼠标位置记录量,用于窗口拖动实现
+        self.mouse_pos = None
+        # 设置窗口无边框
+        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+        # 设置窗口透明度
+        self.setWindowOpacity(0.97)
+        # 设置窗口大小固定
+        self.setFixedSize(self.width(), self.height())
+        # 初始化按钮控件
+        self.initButtons()
+        # 初始化列表控件
+        self.initList()
+        # 初始化label控件
+        self.initLabel()
+
+    def initButtons(self):
+        self.minButton.setStyleSheet(min_style)
+        self.closeButton.setStyleSheet(close_style)
+        self.minButton.setIcon(QIcon("./resources/min.png"))
+        self.closeButton.setIcon(QIcon("./resources/close.png"))
+        self.minButton.clicked.connect(self.showMinimized)
+        self.closeButton.clicked.connect(self.close)
+
+    def initList(self):
+        for interface in self._interfaces:
+            listItem = customQListWidgetItem(interface.name)
+            self.listWidget.addItem(listItem)
+            self.listWidget.setItemWidget(listItem, listItem.widget)
+
+    def initLabel(self):
+        statuspix = QPixmap("./resources/ucas.png")
+        statuspix = statuspix.scaled(QSize(self.ucasLabel.width(), self.ucasLabel.height()),
+                                     Qt.AspectRatioMode.KeepAspectRatio,
+                                     Qt.TransformationMode.SmoothTransformation
+                                     )
+        self.ucasLabel.setPixmap(statuspix)
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        """
+        鼠标按下时记录坐标
+        :type event: QMouseEvent
+        """
+        self.mouse_pos = event.globalPosition()
+
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        """
+        鼠标移动时计算移动距离
+        :type event: QMouseEvent
+        """
+        if self.mouse_pos:
+            diff = event.globalPosition() - self.mouse_pos
+            self.move(self.pos() + diff.toPoint())
+            self.mouse_pos = event.globalPosition()
+
+    def mouseReleaseEvent(self, event: QMouseEvent) -> None:
+        """
+        鼠标释放时清空坐标
+        :type event: QMouseEvent
+        """
+        self.mouse_pos = None
+
+
+class customQListWidgetItem(QListWidgetItem):
+    def __init__(self, iface_name):
+        super().__init__()
+        # 自定义item中的widget 用来显示自定义的内容
+        self.widget = QWidget()
+        # 设置布局
+        self.hboxLayout = QHBoxLayout()
+        # 设置label
+        self.nameLabel = QLabel()
+        self.nameLabel.setText(iface_name)
+        self.statusLabel = QLabel()
+        statuspix = QPixmap("./resources/status_red.png")
+        statuspix = statuspix.scaled(QSize(20, 20),
+                                     Qt.AspectRatioMode.KeepAspectRatio,
+                                     Qt.TransformationMode.SmoothTransformation
+                                     )
+        self.statusLabel.setPixmap(statuspix)
+        self.bytesLabel = QLabel()
+        self.hboxLayout.addWidget(self.nameLabel)
+        self.hboxLayout.addWidget(self.statusLabel)
+        self.hboxLayout.addWidget(self.bytesLabel)
+
+        # 设置widget的布局
+        self.widget.setLayout(self.hboxLayout)
+        # 设置自定义的QListWidgetItem的sizeHint，不然无法显示
+        self.setSizeHint(self.widget.sizeHint())
+
+
---
+++ views/MainWindowView.py
@@ -0,0 +1,144 @@
+import scapy.interfaces
+import scapy.arch.windows
+from PyQt6.QtCore import QEvent, QSize, Qt, pyqtSlot
+from PyQt6.QtGui import QIcon, QMouseEvent, QPixmap
+from PyQt6.QtWidgets import QApplication, QPushButton, QAbstractItemView, QHBoxLayout
+
+from views.primitive_views.MainWindowView_pre import *
+from qt_material import apply_stylesheet
+from custom.style import min_style, close_style, normalButton_style
+import sys
+
+
+class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        # 初始化UI
+        self.hboxLayout = None
+        self.setupUi(self)
+        # 设置鼠标位置记录量,用于窗口拖动实现
+        self.mouse_pos = None
+        # 设置窗口无边框
+        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+        # 设置窗口透明度
+        self.setWindowOpacity(0.97)
+        # 设置窗口大小固定
+        self.setFixedSize(self.width(), self.height())
+        # 初始化按钮控件
+        self.initButtons()
+        # 初始化表格控件
+        self.initTable()
+        # 初始化Label控件
+        self.initLabel()
+
+    def initButtons(self) -> None:
+        """
+        初始化按钮控件
+        :rtype: None
+        """
+        # 单独设置最小化、关闭窗口按钮以及filter按钮的样式
+        self.minButton.setStyleSheet(min_style)
+        self.closeButton.setStyleSheet(close_style)
+        self.minButton.setIcon(QIcon("./resources/min.png"))
+        self.closeButton.setIcon(QIcon("./resources/close.png"))
+        self.doFilterButton.setProperty('class', 'success')
+
+        # 设置按钮图标的QPixmap
+        catchpix = QPixmap("./resources/start.png")
+        catchpix = catchpix.scaled(QSize(80, 80))
+        pausepix = QPixmap("./resources/pause.png")
+        pausepix = pausepix.scaled(QSize(80, 80))
+        restartpix = QPixmap("./resources/restart.png")
+        restartpix = restartpix.scaled(QSize(80, 80))
+        openFilepix = QPixmap("./resources/openFiles.png")
+        openFilepix = openFilepix.scaled(QSize(80, 80))
+        prepix = QPixmap("./resources/pre.png")
+        prepix = prepix.scaled(QSize(80, 80))
+        postpix = QPixmap("./resources/post.png")
+        postpix = postpix.scaled(QSize(80, 80))
+        toppix = QPixmap("./resources/top.png")
+        toppix = toppix.scaled(QSize(80, 80))
+        bottompix = QPixmap("./resources/bottom.png")
+        bottompix = bottompix.scaled(QSize(80, 80))
+        # 将QPixmap应用到按钮
+        self.catchButton.setIcon(QIcon(catchpix))
+        self.pauseButton.setIcon(QIcon(pausepix))
+        self.restartButton.setIcon(QIcon(restartpix))
+        self.openFileButton.setIcon(QIcon(openFilepix))
+        self.jump2preButton.setIcon(QIcon(prepix))
+        self.jump2nextButton.setIcon(QIcon(postpix))
+        self.jump2topButton.setIcon(QIcon(toppix))
+        self.jump2tailButton.setIcon(QIcon(bottompix))
+        # 功能按钮应用自定义style（增加hover样式）
+        for button in self.findChildren(QPushButton):
+            if button.objectName() not in ["closeButton", "minButton", "ucasLogoButton"]:
+                button.setStyleSheet(normalButton_style)
+        self.catchButton.setStyleSheet(normalButton_style)
+
+    def initTable(self) -> None:
+        """
+        对QTableWidget的初始化
+        :rtype: None
+        """
+        # 设置表格单元不可编辑
+        self.tableWidget.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+        # 取消表格默认行号
+        self.tableWidget.verticalHeader().setHidden(True)
+        # 根据现有窗口设置表头单元格宽度
+        self.tableWidget.horizontalHeader().resizeSection(0, 70)
+        self.tableWidget.horizontalHeader().resizeSection(1, 80)
+        self.tableWidget.horizontalHeader().resizeSection(2, 140)
+        self.tableWidget.horizontalHeader().resizeSection(3, 140)
+        self.tableWidget.horizontalHeader().resizeSection(4, 120)
+        self.tableWidget.horizontalHeader().resizeSection(5, 100)
+        self.tableWidget.horizontalHeader().resizeSection(6, 310)
+        headerItemTooltips = ["序号", "捕获时间", "源IP地址", "目的IP地址", "协议", "长度(字节)", "详细"]
+        for idx, tip in zip(range(7), headerItemTooltips):
+            item = self.tableWidget.horizontalHeaderItem(idx)
+            item.setToolTip(tip)
+
+    def initLabel(self) -> None:
+        """
+        对QLabel控件初始化
+        :rtype: None
+        """
+        ucasPixmap = QPixmap("./resources/ucas.png")
+        ucasPixmap = ucasPixmap.scaled(self.ucasLogo.width() - 3, self.ucasLogo.height() - 3,
+                                       Qt.AspectRatioMode.KeepAspectRatio,
+                                       Qt.TransformationMode.SmoothTransformation)
+        self.ucasLogo.setPixmap(ucasPixmap)
+        self.textEdit.verticalScrollBar().setVisible(False)
+        self.textEdit.verticalScrollBar().setDisabled(True)
+        self.textEdit.setReadOnly(True)
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        """
+        鼠标按下时记录坐标
+        :type event: QMouseEvent
+        """
+        self.mouse_pos = event.globalPosition()
+
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        """
+        鼠标移动时计算移动距离
+        :type event: QMouseEvent
+        """
+        if self.mouse_pos:
+            diff = event.globalPosition() - self.mouse_pos
+            self.move(self.pos() + diff.toPoint())
+            self.mouse_pos = event.globalPosition()
+
+    def mouseReleaseEvent(self, event: QMouseEvent) -> None:
+        """
+        鼠标释放时清空坐标
+        :type event: QMouseEvent
+        """
+        self.mouse_pos = None
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    apply_stylesheet(app, theme='light_amber.xml')
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec())
